deea67162ea114835a0ec0a36c9729a65f621b26
dylan7rdgz <56861639+dylan7rdgz@users.noreply.github.com>
2023-12-19 15:49:35 +0530

    Create candidates.js

---
+++ Interviews/candidates.js
@@ -0,0 +1,59 @@
+/*
+
+Snehal:
+1.how can we cancel inline spacing
+2.react features
+3.HOC componets
+4.How to remove duplicates from array
+5.diff bet Let and Const
+6.what is arrow functions and how we use in react
+
+7. Randomly generate n number of characters from given character set (such as uppercase,lowecase,numbers)and no repeated characters are allowed in generation verification code string
+1) first write an algorithm to generate a non repeating array of 26 english characters, numbers and symbol
+
+Ritesh:
+1. position: fixed 
+Under what circumstances will the locking effect be lost?
+2. Explain what event delegation is and what are its advantages?
+3. What are Closures in JavaScript?
+4. What's the output?
+function sum(a, b) { return a + b; } sum(1, '2');
+5. What does "map" do? Please explain the difference between forEach” and "map".
+6. How do you remove Duplicates from a JavaScript Array?
+
+
+"Read the following code and tell the result printed by the browser and the final result rendered by the page
+
+
+Question 1
+
+const [val, setVal] = useState<number>(0)
+useEffect(() => {
+setVal(val + 1)
+console.log(val)
+setVal(10)
+console.log(val)
+setTimeout(() => {
+const newValue = val + 1
+setVal(newValue)
+console.log(newValue)
+console.log(val)
+}, 0)
+}, [])
+return (
+<div>{val}</div>
+);
+"
+
+
+
+Question 2
+Given a string, please find the length of the longest substring that does not contain repeated characters.
+Input: ""abcabcbb""
+Output: 3
+Explanation: Because the longest substring without repeated characters is ""abc"", its length is 3.
+Input: ""bbbbb""
+Output: 1
+Explanation: Because the longest substring without repeated characters is ""b"", its length is 1."
+
+*/
